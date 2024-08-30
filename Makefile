7c6a99c84cc5eedff9c70c60bf0f89f839a93e96
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-08-30 17:06:12 -0300

    chore: add make i with install.sh

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
-g:
-	python3 generate-synthetic-data.py
+SHELL = /bin/bash
+venv: 
+	python3 -m venv venv
+	venv/bin/pip install -r requirements.txt
+g: venv
+	source venv/bin/activate && python3 generate-synthetic-data.py
+i:
+	chmod +x scripts/install.sh && scripts/install.sh && git submodule update --init --recursive 
+
---
+++ scripts/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Check if Python 3 is installed
+if command -v python3 &>/dev/null; then
+    echo "Python 3 já está instalado."
+    python3 --version
+else
+    echo "Python 3 não está instalado. Instalando Python 3..."
+    sudo apt-get update
+    sudo apt-get install -y python3
+fi
+
+# Check if pip3 is installed
+if command -v pip3 &>/dev/null; then
+    echo "pip3 já está instalado."
+else
+    echo "pip3 não está instalado. Instalando pip3..."
+    sudo apt-get install -y python3-pip
+fi
+
+# Create and activate a virtual environment
+echo "Criando e ativando um ambiente virtual..."
+python3 -m venv venv
+source venv/bin/activate
+
+# Display Python 3 and pip3 versions
+python3 --version
+pip3 --version
+
+# Install dependencies with pip3
+echo "Instalando bibliotecas pandas e faker..."
+pip3 install pandas faker
+
+echo "Instalação concluída!"
+echo "Siga para o próximo passo."
+
+
